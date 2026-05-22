efd831661f958bae0f43ec78a7971cc1af455ebd
Stay-16 <swdrakula2005@gmail.com>
2026-05-22 10:30:34 +0100

    Delete bot 2 directory

--- bot 2/RUN_BACKEND_24_7.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "F:\Action!\Trade\op\New folder"
-echo Starting Quotex AI Desk backend on http://127.0.0.1:8000
-"C:\Users\Abde\AppData\Local\Programs\Python\Python312\python.exe" run_webapp.py
-pause
